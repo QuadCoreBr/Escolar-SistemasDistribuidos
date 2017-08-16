20779f8b64b4a28db911a482c99f48f5381a3dab
Julio Diaz Rodriguez <jdiaz@lumbrera.com.mx>
2017-08-16 13:40:34 -0500

    avance en comunicacion entrte funciones

--- Practica1/frontend-electron/windows/invertedThreadClocks.js
+++ Practica1/frontend-electron/windows/invertedThreadClocks.js
@@ -4,12 +4,25 @@ $(document).ready(function() {
     
 });
 
-function setTime(nombreReloj){
-    console.log(nombreReloj);
+var sockets = new Array;
+
+function conectarReloj(dataClock){
+    console.log(dataClock.id);
     var socket = io.connect('http://127.0.0.1:3000');
     socket.on('connect', function(data) {
-        socket.on('substract', function(data) {
-            console.log('me han quitado un segundo' + nombreReloj);
+        var moment =  $("#"+dataClock.id).attr('value');
+        socket.emit('join',{id:dataClock.id ,moment:moment});
+        socket.on('substract', function (data) {
+            console.log("me han quitado un seg");
         });
     });
+    sockets.push(socket);
+}
+
+function setTime(data){
+    var idReloj = data.id;
+    // console.log(idReloj);
+    var moment =  $("#"+idReloj).attr('value');
+    // console.log(moment);
+    sockets[idReloj-1].emit('setTime',moment);
 }
